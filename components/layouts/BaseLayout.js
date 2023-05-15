7768709e3ea8d0a05cd99f861da4d45718344221
abayasaye <abay400@gmail.com>
2023-05-15 17:43:46 +0300

    updating

--- components/layouts/BaseLayout.js
+++ components/layouts/BaseLayout.js
@@ -1,12 +1,16 @@
-import Header from "../shared/Header";
+import Header from '../shared/Header';
 
-const BaseLayout = ({ children }) => {
+const BaseLayout = ({ className, children }) => {
   return (
-    <>
+    <div className="layout-container">
       <Header />
-      {children}
-    </>
-  );
-};
+      <main className={`cover ${className}`}>
+        <div className="wrapper">
+          {children}
+        </div>
+      </main>
+    </div>
+  )
+}
 
 export default BaseLayout;
