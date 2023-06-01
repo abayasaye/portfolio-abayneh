bfb98738d5f54bf4a6d4632f5f8caf8da50ccc97
abayasaye <abay400@gmail.com>
2023-06-01 10:57:16 +0300

    portfolio updated

--- pages/api/auth/portfolios.js
+++ pages/api/auth/portfolios.js
@@ -1,11 +1,10 @@
 import PortfolioApi from "@/lib/api/portfolios";
 
 
-
 const portfolios = async (req, res) => {
   try {
     const data = req.body;
-    await new PortfolioApi().create(data);
+    await new PortfolioApi().createNewPortfolio(data);
     return res.status(200).json({ message: "Portfolios created successfully" });
   } catch (e) {
     return res.status(e.status || 400).end(e.message);
