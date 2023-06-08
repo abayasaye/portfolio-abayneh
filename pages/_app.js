0d71454c5a9b01c64133f5803a03840236293437
abayasaye <abay400@gmail.com>
2023-06-08 15:49:20 +0300

    editor

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-toastify/dist/ReactToastify.css";
 import "react-datepicker/dist/react-datepicker.css";
+import "slate-simple-editor/dist/index.css"
 import "../styles/main.scss";
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
--- pages/blogs/editor/index.js
+++ pages/blogs/editor/index.js
@@ -1,13 +1,19 @@
 import BaseLayout from "@/components/layouts/BaseLayout";
 import BasePage from "@/components/BasePage";
 import withAuth from "@/hoc/withAuth";
+import { Editor } from "slate-simple-editor";
+
 const BlogsEditor = ({ data, loading }) => {
+  const saveBlog = (data) => {
+    console.log(data);
+  };
   return (
     <BaseLayout user={data} loading={loading}>
-      <BasePage header='Blogs Editor'>
+      <BasePage>
+        <Editor onSave={saveBlog} />
         <h1>BlogsEditor page</h1>
       </BasePage>
     </BaseLayout>
   );
 };
-export default withAuth(BlogsEditor)('admin');
+export default withAuth(BlogsEditor)("admin");
