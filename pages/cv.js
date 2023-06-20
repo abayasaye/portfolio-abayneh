d1805b7fcc38362706c1d6e22283032069f987ee
abayasaye <abay400@gmail.com>
2023-06-20 12:02:40 +0300

    cv-implementation

--- pages/cv.js
+++ pages/cv.js
@@ -1,12 +1,17 @@
 import BaseLayout from "@/components/layouts/BaseLayout";
 import BasePage from "@/components/BasePage";
 import { useGetUser } from "@/actions/user";
-
+import { Row, Col } from "reactstrap";
 const Cv = () => {
   const { data, loading } = useGetUser();
   return (
     <BaseLayout user={data} loading={loading}>
       <BasePage>
+      <Row className="mt-3">
+        <Col md={{size: 8, offset: 2}} >
+          <iframe style={{width:'100%', height: '800px'}} src="/cv-abayneh.pdf"/>
+        </Col>
+      </Row>
         <h1>cv page</h1>
       </BasePage>
     </BaseLayout>
