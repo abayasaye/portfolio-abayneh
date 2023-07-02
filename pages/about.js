d463ccc7e92ff67958bccb1e02aeb3784870de79
abayasaye <abay400@gmail.com>
2023-07-02 17:02:37 +0300

    pages fix

--- pages/about.js
+++ pages/about.js
@@ -8,28 +8,28 @@ const About = () => {
   const { data, loading } = useGetUser();
 
   useEffect(() => {
-    return ()=>{
+    return () => {
       window.__isAboutLoaded = true;
-    }
+    };
   }, []);
 
-  const createFadeInClass = ()=>{
-    if(typeof window !== 'undefined'){
-      return window.__isAboutLoaded? '':'fadein';
+  const createFadeInClass = () => {
+    if (typeof window !== "undefined") {
+      return window.__isAboutLoaded ? "" : "fadein";
     }
 
-    return 'fadein';
-  }
+    return "fadein";
+  };
   return (
     <BaseLayout user={data} loading={loading}>
-      <BasePage 
-      title="About Me - Abayneh Asaye"
-      className="about-page">
+      <BasePage title="About Me - Abayneh Asaye" className="about-page">
         <Row className="mt-5">
           <Col md="6">
             <div className="left-side">
               <h1 className={`title ${createFadeInClass()}`}>Hello, Welcome</h1>
-              <h4 className={`subtitle ${createFadeInClass()}`}>To About Page</h4>
+              <h4 className={`subtitle ${createFadeInClass()}`}>
+                To About Page
+              </h4>
               <p className={`subsubTitle ${createFadeInClass()}`}>
                 Feel free to read short description about me.
               </p>
